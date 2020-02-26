798c5933f3c6ac9d760ca555c8b0d7bfd87e10d7
Nicolas KEITA <nicolas.keita@epitech.eu>
2020-02-26 14:27:49 +0100

    add: first window

--- src/com/example/pendu/Main.java
+++ src/com/example/pendu/Main.java
@@ -1,7 +1,8 @@
-package com.example.helloworld;
+package com.example.pendu;
 
-public class HelloWorld {
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        Window window = new Window();
     }
 }
---
+++ src/com/example/pendu/Window.java
@@ -0,0 +1,12 @@
+package com.example.pendu;
+import javax.swing.JFrame;
+
+public class Window extends JFrame {
+        public Window() {
+                this.setTitle("My_Pendu_Hub");
+                this.setSize(1100, 700);
+                this.setLocationRelativeTo(null);
+                this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                this.setVisible(true);
+        }
+}
